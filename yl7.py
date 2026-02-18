854155ffdc3064f602e3005c05e30d0419987167
svenvester <svenvester10@gmail.com>
2026-02-18 14:03:54 +0200

    Remove even/odd number checking program

--- yl7.py
+++ yl7.py
@@ -1,9 +1,5 @@
-#Kirjuta programm, mis ütleb, kas kasutaja poolt etteantud täisarv on paarisarv või mitte. (paarisarvu mõiste - odd/even)
-
-# Küsi kasutajalt täisarvu
 arv = int(input("Sisesta täisarv: "))
 
-# Kontrolli, kas arv on paarisarv
 if arv % 2 == 0:
     print("Number on paarisarv.")
 else:
